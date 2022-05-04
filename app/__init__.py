6e2c052b54c49aa01f2a4f5435e106fa9fa1cd20
Brandon Semilla <semibran@gmail.com>
2022-05-04 00:32:07 -0700

    remove circular import

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,5 @@ Defines logic for app initialization.
 """
 
 from flask import Flask
-from app import views
 
 app = Flask(__name__)
-
-# from app import database_push
-# from app import admin_views
