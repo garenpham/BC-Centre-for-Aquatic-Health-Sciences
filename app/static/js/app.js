05af0cc3dbb39be8200d89076224189a9475c7e2
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-10-14 02:08:47 -0700

    navMobile_navHighlight

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,21 @@
-console.log("Hello from app.js!");
+function highlight() {
+	var pageurl = location.href;
+	var dnl = document.getElementsByTagName("a");
+	for (i = 0; i < dnl.length; i++) {
+		var x = dnl.item(i);
+		if (x.href == pageurl) {
+			if (
+				!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
+			) {
+				// what you want to run in desktop
+				x.style.fontWeight = "bold";
+				x.style.color = "white";
+			} else {
+				// mobile
+				x.style.color = "white";
+			}
+		}
+	}
+}
+
+window.onload = highlight;
