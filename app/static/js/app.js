5d3b4dcd549b9befb4e916069cebddc0c83b9a73
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-10-14 02:51:36 -0700

    navLogoutDropdown

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,7 @@
 function highlight() {
 	var pageurl = location.href;
 	var dnl = document.getElementsByTagName("a");
+	var itemDropdown = document.getElementById("itemDropdown");
 	for (i = 0; i < dnl.length; i++) {
 		var x = dnl.item(i);
 		if (x.href == pageurl) {
@@ -10,9 +11,11 @@ function highlight() {
 				// what you want to run in desktop
 				x.style.fontWeight = "bold";
 				x.style.color = "white";
+				itemDropdown.setAttribute("style", "text-align: end !important");
 			} else {
 				// mobile
 				x.style.color = "white";
+				itemDropdown.setAttribute("style", "text-align: center !important");
 			}
 		}
 	}
