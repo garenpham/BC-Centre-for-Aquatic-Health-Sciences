98cef3d8f5506e55f314d1c1820f741e13b5b5d7
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-10-17 09:44:12 -0700

    rename var

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,24 +1,24 @@
 function highlight() {
 	var pageurl = location.href;
-	var dnl = document.getElementsByTagName("a");
+	var navlinks = document.getElementsByTagName("a");
 	var itemDropdown = document.getElementById("itemDropdown");
 	var dropdownNav = document.getElementById("dropdownNav");
 
-	for (i = 0; i < dnl.length; i++) {
-		var x = dnl.item(i);
-		if (x.href == pageurl) {
+	for (i = 0; i < navlinks.length; i++) {
+		var currentPage = navlinks.item(i);
+		if (currentPage.href == pageurl) {
 			if (
 				!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
 			) {
 				// what you want to run in desktop
-				x.style.fontWeight = "bold";
-				x.style.color = "white";
-				itemDropdown.setAttribute("style", "text-align: end !important");
+				currentPage.style.fontWeight = "bold";
+				currentPage.style.color = "white";
+				itemDropdown.setAttribute("style", "text-align: center");
 			} else {
 				// mobile
-				x.style.color = "white";
-				itemDropdown.setAttribute("style", "text-align: center !important");
-				dropdownNav.setAttribute("style", "display: block !important");
+				currentPage.style.color = "white";
+				itemDropdown.setAttribute("style", "text-align: center ");
+				dropdownNav.setAttribute("style", "display: block");
 			}
 		}
 	}
