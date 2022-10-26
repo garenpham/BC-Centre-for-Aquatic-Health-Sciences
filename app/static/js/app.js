0ec1a8705cc6d5c32db5ec732af4a3783191268a
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-10-26 01:40:27 -0700

    current nav highlight bg

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,24 +1,24 @@
 function highlight() {
 	var pageurl = location.href;
 	var navlinks = document.getElementsByTagName("a");
-	var dropdownNav = document.getElementById("dropdownNav");
 	var navSmTitle = document.getElementById("navSmTitle");
+	var navBg = document.getElementsByClassName("nav-bg");
 
 	for (i = 0; i < navlinks.length; i++) {
 		var currentPage = navlinks.item(i);
 		if (currentPage.href == pageurl) {
+			currentPage.style.color = "white";
+			navSmTitle.innerHTML = currentPage.innerHTML;
+			navBg.item(i).style.backgroundColor = "black";
 			if (
 				!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
 			) {
 				// what you want to run in desktop
 				currentPage.style.fontWeight = "bold";
-				currentPage.style.color = "white";
 			} else {
 				// mobile
-				currentPage.style.color = "white";
-				dropdownNav.setAttribute("style", "display: block");
-				navSmTitle.innerHTML = currentPage.innerHTML;
 			}
+		} else {
 		}
 	}
 }
@@ -28,14 +28,18 @@ window.onload = highlight;
 var navbar_toggler = document.querySelector(".navbar-toggler");
 var closeIcon = document.getElementById("closeIcon");
 var main = document.querySelector("main");
+var body = document.querySelector("body");
 var navCollapse = document.querySelector(".navbar-collapse");
 
 navbar_toggler.addEventListener("click", function () {
 	closeIcon.classList.toggle("active");
 	if (closeIcon.classList.contains("active")) {
 		navCollapse.classList.remove("active");
+		main.classList.add("active");
+		body.classList.add("active");
 	} else {
 		navCollapse.classList.add("active");
+		main.classList.remove("active");
+		body.classList.add("active");
 	}
-	main.classList.toggle("active");
 });
