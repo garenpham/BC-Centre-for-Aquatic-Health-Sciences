ffadc355d64a6a87cea93bbd6ac296aa31e9d92c
informationvulture <schoolemailnm1@gmail.com>
2022-10-26 20:30:26 -0700

    Add improved R script for data vis.

---
+++ visual_data.R
@@ -0,0 +1,27 @@
+library(ggplot2)
+library(dplyr)
+library(stringr)
+
+# Load data
+
+# This will have to be change later 2022-10-26
+data <- read.csv(file = '/mnt/c/Users/Noje/Downloads/new_data_query.csv')
+data$date_collected <- as.Date(data$date_collected)
+
+
+specify_decimal <- function(x, k) trimws(format(round(x, k), nsmall=k))
+#Plot
+png(filename = "/mnt/c/Users/Noje/Downloads/new_data1.png")
+data %>%
+  tail(10) %>%
+  ggplot( aes(x=date_collected, y=fraction_total_reads, color=name)) +
+  xlab(str_glue("Daterange \n {data$date_collected[1]} to {data$date_collected[nrow(data)]}")) + ylab("Relative Abundance") +
+    geom_line() +
+    geom_point() +
+    geom_text(
+    data = . %>% filter(fraction_total_reads == data$fraction_total_reads),
+    aes(label = specify_decimal(fraction_total_reads, 4)),
+    vjust = "inward", hjust = "inward",
+    show.legend = FALSE) +
+    ggtitle("Relative Abundance Graph (Species/Date range)")
+dev.off()
