d2c9b9e509c375e5b3f1986486ae099545965021
informationvulture <schoolemailnm1@gmail.com>
2022-11-02 17:53:16 -0700

    Add hotfix for R bug from last commit

--- app/r/trend_graphs.R
+++ app/r/trend_graphs.R
@@ -16,8 +16,7 @@ png(filename = "app/static/img/trend_data.png",
 data %>%
   tail(100) %>%
   ggplot(aes(x = date_collected, y = fraction_total_reads, color = name)) +
-  xlab(str_glue("Daterange \n {data$date_collected[1]} to {data$date_collected[nrow(data)]}"))
-    + ylab("Relative Abundance") +
+  xlab(str_glue("Daterange \n {data$date_collected[1]} to {data$date_collected[nrow(data)]}")) + ylab("Relative Abundance") +
     geom_line() +
     geom_point() +
     geom_text(
